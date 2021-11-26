6e612c92671360e3a94cc7f71928ce3e3bd47c7e
tiagobviegas <tiagobviegas@gmail.com>
2021-11-26 15:23:06 -0300

    fazendo exercicios de variaveis

---
+++ fundamentos/bloco-04-introducao-ao-javascript/dia-01-variaveis/dia-01.js
@@ -0,0 +1,32 @@
+// EXERCÍCIOS | VARIÁVEIS
+/*
+Vamos praticar o que aprendemos no vídeo? 💪
+Abra o seu VS Code e faça os exercícios a seguir.
+Dica: Com o plugin code runner que vimos no vídeo você pode executar o seu código no VS
+Code com o atalho ctrl + alt + n.
+*/
+
+
+// 1) Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+var myName = "Tiago Viegas";
+
+// 2) Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+const birthCity = "Brasília-DF";
+
+// 3) Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+var birthYear = 1981;
+
+// 4) Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+console.log(myName, birthCity, birthYear)
+
+// 5) Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+var birthYear = 2030;
+console.log(myName, birthCity, birthYear)
+// o ano de nascimento impresso na tela foi o valor da variável birthYear atualizada;
+
+// 6) Altere o valor atribuído à constante birthCity . Faça um console.log(birthCity)
+const birthCity = "Vancouver";
+console.log(birthCity);
+
+
+novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔
