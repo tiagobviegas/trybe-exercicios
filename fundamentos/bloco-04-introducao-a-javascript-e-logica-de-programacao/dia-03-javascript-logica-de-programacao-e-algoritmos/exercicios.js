86092527798c1e02481b29ecb5a30a145b3f5833
tiagobviegas <tiagobviegas@gmail.com>
2021-11-30 14:19:18 -0300

    atualizando o computador

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -0,0 +1,50 @@
+// APROFUNDE SEUS CONHECIMENTOS
+// 
+// Leia atentamente os enunciados e faça o que se pede!
+// 
+//   Recomendamos que você utilize o debugger durante a realização dos exercícios, desse modo será mais fácil acompanhar o comportamento do código e entender o que ocorre em cada uma das linhas. Para saber mais sobre como utilizar o debugger , acesse nosso conteúdo sobre isso.
+// 
+// 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+// 
+//   n = 5
+// 
+//   *****
+//   *****
+//   *****
+//   *****
+//   *****
+// 
+// 
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+// 
+//   n = 5
+//   
+//   *
+//   **
+//   ***
+//   ****
+//   *****
+// 
+// 
+// 3- Agora inverta o lado do triângulo. Por exemplo:
+//   
+//   n = 5
+//   
+//       *
+//      **
+//     ***
+//    ****
+//   *****
+// 
+//   Atenção! Note que esse exercício é bem mais complexo que o anterior! Não basta, aqui, imprimir somente asteriscos. Você precisará de uma lógica para imprimir espaços também.
+// 
+// 
+// 4- Depois, faça uma pirâmide com n asteriscos de base:
+// 
+//   n = 5
+// 
+//     *
+//    ***
+//   *****
+// 
+// 
