7726db8d1533c7df4671fe5db84e394883355228
tiagobviegas <tiagobviegas@gmail.com>
2021-12-06 23:03:57 -0300

    exercicios do dia 4-1 atualizados

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios.js
@@ -47,7 +47,7 @@
 // EXERCÍCIOS | CONDICIONAIS IF E ELSE
 //
 //  Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100;
-    const nota = 85;
+//    const nota = 85;
 //  Implemente uma lógica que verifique se a pessoa candidata foi aprovada, reprovada ou se ela está na lista de espera. Para isso, considere as seguintes informações:
 //  > Se a nota for maior ou igual a 80, imprima "Parabéns, você foi aprovada(o)!"
 //  > Se a nota for menor que 80 e maior ou igual a 60, imprima "Você está na nossa lista de espera"
