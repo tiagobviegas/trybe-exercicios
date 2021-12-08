5fe15f3e5fa633e73c5fff44fc758af4da6cc87a
tiagobviegas <tiagobviegas@gmail.com>
2021-12-08 13:36:29 -0300

    resolvendo exercicios do dia 4-3

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-pre-aula.js
@@ -0,0 +1,49 @@
+//  EXERCÍCIO 1 | ARRAYS (LISTAS)
+//
+//  Exercício 1
+//  Obtenha o valor "Serviços" do array menu:
+//
+//  let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//  let menuServices = menu[1];
+//
+//  console.log(menuServices);
+//
+//
+//  Exercício 2
+//  Procure o índice do valor "Portfólio" do array menu :
+//  
+//  let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//  let indexOfPortfolio = menu.indexOf('Portfólio');
+//  
+//  console.log(indexOfPortfolio);
+//  
+//  
+//  Exercício 3
+//  Adicione o valor "Contato" no final do array menu :
+//
+//  let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//  menu.push('Contato')
+//  console.log(menu);
+//
+//
+//  EXERCÍCIOS | FOR
+//
+//  Exercício 1
+//  Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
+//
+//  let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+//  for (let i = 0; i < groceryList.length; i += 1) {
+//    console.log(groceryList[i]);
+//  }
+//
+//  EXERCÍCIOS | FOR OF
+//
+//  Exercício 1
+//  Utilize o for/of para imprimir os elementos da lista names com o console.log() :
+//
+//  let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+//  for (let nomes of names) {
+//    console.log(nomes);
+//  }
+//
+//
