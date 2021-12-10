08f387bb238e77ecfe2f7105dc6e4c47265f677b
tiagobviegas <tiagobviegas@gmail.com>
2021-12-10 14:24:19 -0300

    atualiza resolução de exercícios pos aulas

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-pos-aula.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-pos-aula.js
@@ -20,9 +20,13 @@
 //
 //  let numero = 1;
 //  for (let i = 10; i > 1; i -= 1) {
-//    numero = numero * i;
+//    numero *= i;
 //  }
 //  console.log(numero);
 //
 //
-//
+//  2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+//  
+    let word = 'banana';
+    console.log(word.split('').reverse().join(''));
+//  
