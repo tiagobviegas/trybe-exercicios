ff97a2874e4055fda4b6df4124913ef98fb22cec
tiagobviegas <tiagobviegas@gmail.com>
2021-12-15 13:00:00 -0300

    soluciona exercicios bonus do dia 4.2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios-pos-aula.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios-pos-aula.js
@@ -110,3 +110,49 @@
 //    result = newArray[i] / 2;
 //    console.log(result);
 //  }
+//
+//
+//  BÔNUS
+//
+//  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//
+//  1) Ordene o array numbers em ordem crescente e imprima seus valores;
+//
+//  for (let i = 1; i < numbers.length; i += 1) {
+//    for (let c = 0; c < i; c += 1) {
+//      if (numbers[c] > numbers[i]) {
+//        let orderedNumbers = numbers[i];
+//        numbers[i] = numbers[c];
+//        numbers[c] = orderedNumbers;
+//      }
+//    }
+//  }
+//  console.log(numbers);
+//
+//
+//  2) Ordene o array numbers em ordem decrescente e imprima seus valores;
+//
+//  for (let i = 1; i < numbers.length; i += 1) {
+//    for (let c = 0; c < i; c += 1) {
+//      if (numbers[i] > numbers[c]) {
+//        let orderedNumber = numbers[c];
+//        numbers[c] = numbers[i];
+//        numbers[i] = orderedNumber;
+//      }
+//    }
+//  }
+//  console.log(numbers);
+//
+//
+//  3) Agora crie um novo array a partir do array numbers , sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push . O resultado deve ser o array abaixo:
+//
+//  [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]
+//
+//  let newNumbers = [];
+//  for (let i = 1; i < numbers.length; i += 1) {
+//    newNumbers.push(numbers[i-1]*numbers[i]);
+//  }
+//  newNumbers.push(numbers[numbers.length-1]*2)
+//  console.log(newNumbers);
+
+  
