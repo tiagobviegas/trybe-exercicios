4d3b059d3b13a64cf55e1b641bd96ccfd0eb204d
tiagobviegas <tiagobviegas@gmail.com>
2021-12-16 12:02:20 -0300

    atualizando exercicios 4.3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-pre-aula.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-pre-aula.js
@@ -8,7 +8,7 @@
 //
 //  Leia atentamente os enunciados e faça o que se pede! Iremos utilizar esse array para realizar os exercícios do 1 ao 7:
 //
-//  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //
 //
 //  1) Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
@@ -25,7 +25,7 @@
 //  2) Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 //
 //    > Primeiro definimos a variável "sum" para armazenamento da soma que acontecerá futuramente;
-//    > Em seguida estabelecemos um laço de repetição "for" cuja contagem de repetições é definida pela variável "i" se iniciando em "0", indicando que a leitura do array "numbers" vai começar pelo primeiro índice do array.
+//    > Em seguida estabelecemos um laço de repetição "for" cuja contagem de repetições é definida pela variável "i" se iniciando em "0", indicando que a leitura do array "numbers" vai começar pelo primeiro índice do array;
 //    > Estabelecemos a condição de repetição do laço para que a ação ocorra enquanto o valor de "i" for menor que o comprimento do array (numbers.length);
 //    > Finalizamos os parâmetros de funcionamento do laço "for" estabelecendo que a cada repetição a variável de contagem "i" será incrementada em "1" até que a condição de repetição seja satisfeita;
 //    > O código continua com a implementação da soma dos valores contidos no array que serão armazenados na variável "sum". A cada repetição do laço "sum" será o valor dela mesma mais o valor do índice do array "numbers";
@@ -40,26 +40,35 @@
 //
 //  3) Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 //
-//    > A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+//    OBS: A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+//    
+//    > Primeiramente criamos uma variável "sum" para guardar a futura soma dos valores dos elementos do array;
+//    > Então fazemos um laço "for" para fazer a leitura de todos os elementos de "numbers", começando a leitura pelo primeiro índice do array com a variável "i" com valor "0". Fazemos o laço de repetição continuar enquanto "i" for menor que o comprimento do array (number.length), incrementando o valor de "i" em "1" a cada repetição;
+//    > Dessa forma estabelecemos que durante esse laço, a cada repetição a variável "sum" manterá o próprio valor e somará o valor do índice do array, determinado por "numbers[i]";
+//    > Após a saída do laço "for" estabelecemos que a variável "arithmeticMean" receberá o valor de sum, que o dividirá pelo número de elementos do array (numbers.length) para encontrar a média aritmética;
+//    > Por fim, usamos console.log para exibir o valor da média aritmética da soma dos elementos do array representada por "arithmeticMean".
 //
 //  let sum = 0;
-//  let arithmeticMean;
 //  for (let i = 0; i < numbers.length; i += 1) {
 //    sum += numbers[i];
-//    arithmeticMean = sum / numbers.length
 //  }
+//  let arithmeticMean = sum / numbers.length;
 //  console.log(arithmeticMean);
 //
 // 
 //  4) Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 //
+//    > Aproveitamos a variável "sum" e as primeiras cinco linhas do código anterior e as utilizamos para fazer a soma dos elementos de "numbers" e obter a média artimética dos seus elementos;
+//    > Criamos antes do início do código a variável "result" que vai exibir a informação das condições conforme as circunstâncias;
+//    > Após o cálculo de média aritmética fazemos um laço de decisão com "if" para avaliar o valor de "arithmeticMean". Se a média for maior que 20, a variável "result" receberá a mensagem 'valor maior que 20', senão "result" receberá a string 'valor menor ou igual a 20';
+//    > E assim, definimos que após a avaliação do resultado da média, a variável "result" contendo a mensagem será exibida via "console.log". 
+//
 //  let sum = 0;
-//  let arithmeticMean;
 //  let result;
 //  for (let i = 0; i < numbers.length; i += 1) {
 //    sum += numbers[i];
-//    arithmeticMean = sum / numbers.length
 //  }
+//  let arithmeticMean = sum / numbers.length;
 //  if (arithmeticMean > 20) {
 //    result = 'valor maior que 20';
 //  } else {
@@ -69,6 +78,11 @@
 //
 //
 //  5) Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+//
+//    > Iniciamos criando a variável "highest" com valor do primeiro índice para ser referência de comparação entre os elementos do array, e para futuramente ser usada para guardar o maior valor de "numbers";
+//    > Então iniciamos um laço de repetição "for" para ler todos os elementos de "numbers", fazendo a contagem ser atribuída a variável "i", que começará valendo "0" e enquanto seu valor for menor que o comprimento de "numbers" (numbers.length), "i" será acrescido de "1" a cada repetição, fazendo mudar a leitura de um índice para o próximo;
+//    > Então fazemos um laço de decisão com "if" estabelecendo que se o índice do array (numbers[i]) for maior do que o maior número (highest) o valor de highest passará a ser o do de "numbers[i]". Essa checagem será feita a cada repetição do laço "for";
+//    > Assim, após o término do laço, usamos console.log para exibir o valor mais alto do array atribuído à variável "highest". 
 //
 //  let highest = 0;
 //  for (let i = 0; i < numbers.length; i += 1) {
@@ -80,6 +94,8 @@
 //
 //
 //  6) Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+//
+//    > Definimos primeiramente uma variável que será usada posteriomente para guardar 
 //
 //  let oddNumbers = 0;
 //  for (let i = 0; i < numbers.length; i += 1) {
