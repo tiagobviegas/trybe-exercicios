4e4ef7a8f917d0487860174aa32cad445c073f41
tiagobviegas <tiagobviegas@gmail.com>
2021-12-20 09:57:12 -0300

    primeiras 2 questoes bonus do dia 4.2 resolvidas

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-pos-aula.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-pos-aula.js
@@ -79,10 +79,100 @@
 //        checkPrime += 1; 
 //      }
 //    }
-//    if(checkPrime <= 2) {
+//    if(checkPrime < 3) {
 //      prime.push(i); 
 //    }   
 //  }
 //  console.log(prime[prime.length - 1]);
 //  
 //
+//  EXERCÍCIOS BÔNUS
+//
+//  1- Agora vamos trabalhar com algumas formas geométricas! Faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+//  
+//  n = 5
+//  
+//  *****
+//  *****
+//  *****
+//  *****
+//  *****
+//
+//
+//  let number = 5;
+//  for (let i = 0; i < number; i += 1) {
+//    let asterisk = '';
+//    for (let c = 0; c < number; c += 1) {
+//      asterisk += '*';
+//    }
+//    console.log(asterisk);
+//    asterisk = '';
+//  }
+//
+//
+//  2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+//
+//  n = 5
+//  
+//  *
+//  **
+//  ***
+//  ****
+//  *****
+//
+//  let asterisk = '';
+//  let number = 5;
+//  for (let i = 0; i < number; i += 1) {
+//    asterisk += '*';
+//    console.log(asterisk);
+//  }
+//
+//
+//  3- Agora inverta o lado do triângulo. Por exemplo:
+//
+//  n = 5
+//  
+//      *
+//     **
+//    ***
+//   ****
+//  *****
+//  
+//  Atenção! Note que esse exercício é bem mais complexo que o anterior! Não basta, aqui, imprimir somente asteriscos. Você precisará de uma lógica para imprimir espaços também.
+//
+//  let asterisk = '*';
+//  let space = '     ';
+//  space -= ' ';
+//  console.log(space);
+//  for(let i = 0; i < n; i += 1) {
+//    for(let c = 0; c < n; c += 1) {
+//      
+//    }
+//  }
+//  
+//  
+//  4- Depois, faça uma pirâmide com n asteriscos de base:
+//  
+//  n = 5
+//  
+//    *
+//   ***
+//  *****
+//  
+//  
+//  5- Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
+//  
+//  Por último, façamos com que a variável seja incrementada com o valor correspondente a cada loop;
+//  n = 7
+//  
+//     *
+//    * *
+//   *   *
+//  *******
+//  
+//  
+//  6- Faça um programa que diz se um número definido numa variável é primo ou não.
+//      > Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele com quaisquer outros números dá resto diferente de zero;
+//      > Dica: você vai precisar de fazer um loop que vá de 0 ao número definido; Além disso, vai precisar de fazer uma checagem a cada iteração e armazenar os resultados em algum lugar.
+//  
+//  
