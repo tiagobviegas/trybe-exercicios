a8423cfbf8f689e27fe4e73f7c6bfc552ee96d96
tiagobviegas <tiagobviegas@gmail.com>
2021-12-31 22:52:20 -0300

    exercicios parte III atualizados

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/scriptIII.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/scriptIII.js
@@ -1,10 +1,10 @@
 /*
 <body>
   <main id="paiDoPai">
-    <section id="pai">
+    <section id="pai">  <<<<<<<<<<<<<<<<<<
       <section id="primeiroFilho"></section>
-      <section id="elementoOndeVoceEsta">
-        <section id="primeiroFilhoDoFilho"></section>
+      <section id="elementoOndeVoceEsta">     <<<<<<<<<<<<<<<<<<
+        <section id="primeiroFilhoDoFilho"></section>     <<<<<<<<<<<<<<<<<<
         <section id="segundoEUltimoFilhoDoFilho"></section>
       </section>
       Atenção!
@@ -33,8 +33,20 @@
     let primeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
     let novoFilhoPrimeiroFilhoDoFilho = document.createElement('p');
       novoFilhoPrimeiroFilhoDoFilho.innerText = 'Filho do Primeiro Filho do Filho';
-    primeiroFilhoDoFilho.appendChild(novoFilhoPrimeiroFilhoDoFilho);
+      primeiroFilhoDoFilho.appendChild(novoFilhoPrimeiroFilhoDoFilho);
 //
 //  4. A partir desse filho criado, acesse terceiroFilho.
     novoFilhoPrimeiroFilhoDoFilho.parentElement.parentElement.nextElementSibling;
-
+//
+//
+//  PARTE III
+//
+//  Remova todos os elementos filhos de paiDoPai exceto pai , elementoOndeVoceEsta e primeiroFilhoDoFilho .
+//
+    let paiDoPai = document.querySelector('#paiDoPai');
+    let pai = document.querySelector('#pai');
+    let primeiroFilho = document.querySelector('#primeiroFilho');
+      pai.removeChild(primeiroFilho);
+      primeiroFilhoDoFilho.removeChild(novoFilhoPrimeiroFilhoDoFilho);
+    
+//
