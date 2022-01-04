b1334fb9cdd99a5c9fc59dcf46cb8bb64ec7f8d0
tiagobviegas <tiagobviegas@gmail.com>
2022-01-04 20:05:56 -0300

    atualiza exercicios

---
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
@@ -0,0 +1,9 @@
+let estados = document.querySelector('#select-estados');
+let listaEstados = ['AC','AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
+
+for (let i = 0; i < listaEstados.length; i += 1) {
+  let itemEstado = document.createElement('option');
+  let estado = listaEstados[i];
+  itemEstado.innerText = estado;
+  estados.appendChild(estado);
+}
