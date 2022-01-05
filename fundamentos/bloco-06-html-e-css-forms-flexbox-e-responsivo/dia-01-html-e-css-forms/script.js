3f558c6bd132d9bdc1e48b5ec722cb818ba9ef4b
tiagobviegas <tiagobviegas@gmail.com>
2022-01-05 00:10:36 -0300

    corrige exercicios do dia 6.1

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
@@ -1,3 +1,8 @@
+//  > Estado - Select
+//    Todos os estados do Brasil;
+//    Utilize estruturas de repetição via JavaScript para gerar os <option>;
+//    Campo obrigatório.
+
 let estados = document.querySelector('#select-estados');
 let listaEstados = ['AC','AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
 
@@ -5,5 +10,5 @@ for (let i = 0; i < listaEstados.length; i += 1) {
   let itemEstado = document.createElement('option');
   let estado = listaEstados[i];
   itemEstado.innerText = estado;
-  estados.appendChild(estado);
+  estados.appendChild(itemEstado);
 }
