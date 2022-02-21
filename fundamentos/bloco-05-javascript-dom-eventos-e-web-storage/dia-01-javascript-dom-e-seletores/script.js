f3d9f99955eb675217ae0bb97584f3347a52bb85
tiagobviegas <tiagobviegas@gmail.com>
2022-02-21 00:09:25 -0300

    refatorando exercicios parte III pos aula

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -1,1 +1,6 @@
-const emergencias = document.getElementsByClassName('emergency-tasks')
+const body = document.getElementById('container');
+  body.style.backgroundColor = '#E8E8E8';
+  body.style.fontFamily = 'Arial';
+
+const headerContainer =  document.getElementById('header-container');
+  headerContainer.style.backgroundColor = '#199C60';
