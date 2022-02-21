14cdcf86f0cd0600937f248683c4d004dc0d7bab
tiagobviegas <tiagobviegas@gmail.com>
2022-02-21 09:49:23 -0300

    refatorando exercicios do dia 5.2 - parte I

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/scriptI.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/scriptI.js
@@ -1,9 +1,5 @@
-//  Diferença entre nextSibling e nextElementSibling 
+// Adicione o código abaixo a uma página HTML e adicione uma tag script . Você deverá fazer tudo a seguir usando somente JavaScript .
 //
-//  console.log(document.getElementById('start').nextSibling) // nó
-//  
-//  console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>  
-//  
 //  <main id="paiDoPai">
 //    <section id="pai">
 //      <section id="primeiroFilho"></section>
@@ -16,24 +12,23 @@
 //      <section id="quartoEUltimoFilho"></section>
 //    </section>
 //  </main>
-//  
+//      
+
 //  1. Acesse o elemento elementoOndeVoceEsta;
-      document.querySelector('#elementoOndeVoceEsta');
-//  2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele;
-      let paiElementoOndeVoceEsta = document.querySelector('#elementoOndeVoceEsta').parentNode;
-        paiElementoOndeVoceEsta.style.color = 'green';
-//  3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
-      let textoprimeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
-        textoprimeiroFilhoDoFilho.innerText = 'texto 1';
+    const ondeEstou = document.getElementById('elementoOndeVoceEsta');
+
+    //  2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele;
+    const paiOndeEstou = ondeEstou.parentElement;
+      paiOndeEstou.style.color = 'red';
+
+//  3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos 
+
 //  4. Acesse o primeiroFilho a partir de pai;
-      let pai = document.querySelector('#pai');
-        pai.firstChild;
+
 //  5. Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta;
-      let elementoOndeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
-        elementoOndeVoceEsta.firstChild;
+
 //  6. Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta;
-        elementoOndeVoceEsta.nextSibling;
+
 //  7. Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta;
-        elementoOndeVoceEsta.nextElementSibling;
+
 //  8. Agora acesse o terceiroFilho a partir de pai.
-        pai.children[2]
