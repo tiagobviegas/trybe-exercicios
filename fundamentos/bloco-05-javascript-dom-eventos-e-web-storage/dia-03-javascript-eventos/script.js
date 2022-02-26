f1a59d3a5ddb2da029bc259ef0fc5d39171cef39
tiagobviegas <tiagobviegas@gmail.com>
2022-02-26 11:58:28 -0300

    tentando resolver o exercicio 10 dos exercicios pos-aula do dia 5.3

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
@@ -212,7 +212,7 @@ createDaysOfTheWeek();
 //
 //   Implemente uma função que adiciona um evento que, ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada.
     
-//   > Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119) .
+//   > Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119).
 
   function setDayTask() {
     let allDays = document.querySelectorAll('.day');
