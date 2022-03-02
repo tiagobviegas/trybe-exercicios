58d599127f0d7ed7ba5cc42212caa4cde7382fd0
tiagobviegas <tiagobviegas@gmail.com>
2022-03-02 03:37:33 -0300

    iniciando exercicios pos aula do dia 6.1

---
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/exercicios-pos-aula/script.js
@@ -0,0 +1,7 @@
+// Agora vamos utilizar um pouco de javascript, para isso crie o arquivo script.js e o referencie no HTML.
+//   Dica: Coloque a tag <script> no final do seu body.
+
+// 3 - Interrompa o comportamento padrão do botão submit utilizando o método preventDefault() . Nossa amiga Carol Silva nos contou um pouco sobre como fazer isso, lembra?
+
+// 4 - Crie um botão que limpe as informações contidas nos campos;
+
