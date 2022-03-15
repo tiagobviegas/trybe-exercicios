0b08a38c38021814d9061a865dff32a1f92d4f87
tiagobviegas <tiagobviegas@gmail.com>
2022-03-15 17:19:00 -0300

    exercicio 3 da parte I concluido

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/parte-I/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/parte-I/script.js
@@ -4,3 +4,5 @@
 // 2 - Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
   const pai = ondeEsta.parentElement;
 
+// 3 - Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
