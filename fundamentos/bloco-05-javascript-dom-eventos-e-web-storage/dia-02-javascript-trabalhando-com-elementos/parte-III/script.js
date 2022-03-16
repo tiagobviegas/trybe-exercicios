889aae6c0ec9b6311a5371485d65fba21de8e42e
tiagobviegas <tiagobviegas@gmail.com>
2022-03-16 16:24:03 -0300

    exercicio da parte III concluido

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/parte-III/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/parte-III/script.js
@@ -1,9 +1,16 @@
 //  Remova todos os elementos filhos de paiDoPai exceto pai , elementoOndeVoceEsta e primeiroFilhoDoFilho.
 //
-let pai = document.getElementById('pai');
-let paiChildren = pai.childNodes;
-for(let i = paiChildren.length; i > 0; i -= 1) {
-  if(paiChildren.id !== 'elementoOndeVoceEsta' && paiChildren.id !== 'primeiroFilhoDoFilho'){
-    pai.removeChild(paiChildren);
+
+const pai = document.getElementById('pai')
+const paiChildren = pai.childNodes;
+const ondeEstou = document.getElementById('elementoOndeVoceEsta')
+const ondeEstouChildren = ondeEstou.childNodes;
+for(let i = paiChildren.length - 1; i > 0; i -= 1){
+  if(paiChildren[i].id !== ondeEstou.id) {
+    paiChildren[i].remove();
+  } else if (paiChildren[i].id === ondeEstou.id) {
+    if(ondeEstouChildren[i].id !== 'primeiroFilhoDoFilho') {
+      ondeEstouChildren[i].remove();
+    }
   }
 }
