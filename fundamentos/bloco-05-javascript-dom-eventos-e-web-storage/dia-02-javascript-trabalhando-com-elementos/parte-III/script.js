65bc8a313f21a7a90ae6cb2e8ec41ecda2bc66e0
tiagobviegas <tiagobviegas@gmail.com>
2022-03-16 18:59:47 -0300

    exercicio da parte III do dia 5.2 concluido

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/parte-III/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/parte-III/script.js
@@ -12,15 +12,3 @@ for (let i = paiChildren.length - 1; i > 0; i -= 1) {
     ondeEstouChildren[i].remove();
   }
 }
-
-const paiChildren = document.getElementById('pai').childNodes;
-const ondeEstou = document.getElementById('elementoOndeVoceEsta');
-const ondeEstouChildren = ondeEstou.childNodes;
-
-for (let i = paiChildren.length - 1; i > 0; i -= 1) {
-  if (paiChildren[i].id !== ondeEstou.id) {
-    paiChildren[i].remove();
-  } else {
-    ondeEstouChildren[i].remove();
-  }
-}
