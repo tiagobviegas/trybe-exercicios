05130ae73c468bc0939f233c0b2002b92fdcef4e
tiagobviegas <tiagobviegas@gmail.com>
2022-03-18 09:38:39 -0300

    exercicio 1.2 da parte II do dia 5.3 concluido

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicios-parte-II/main.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicios-parte-II/main.js
@@ -4,49 +4,37 @@ const thirdLi = document.getElementById('third-li');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('my-spotrybefy');
 
+
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
-
-//  RESPOSTA: Por quê a class .tech adiciona um transform: translateY(-20px) ao primeiro 'li' do 'container'.
-
+//      Resposta: a primeira caixa possui a class "tech", que adiciona o estilo "transform: translateY(-20px)" deslocando a caixa para cima, contra o eixo o fluxo original do eixo secundário de alinhamento do container pai.
+//
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
-  const listItens = document.querySelectorAll('li');
-
-  firstLi.addEventListener('click', setClass);
-  secondLi.addEventListener('click', setClass);
-  thirdLi.addEventListener('click', setClass);
-
-  function setClass (item){
-    for(let i = 0; i < listItens.length; i += 1) {
-      listItens[i].className = 'null';
-    }
-    item.target.className = 'tech';
-  }
-
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
-
-  input.addEventListener('keyup', function () {
-    const item = document.querySelector('.tech');
-      item.innerText = input.value;
-  });
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
 
-  myWebpage.addEventListener('dblclick', function() {
-    window.open("https://tiagobviegas.github.io/project-lessons-learned/index.html");
-  });
-  
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
 
-  myWebpage.addEventListener('mouseover', changeColor);
-  
-  function changeColor(mySpotrybefy) {
-    mySpotrybefy.target.style.color = 'lightBlue';
-  }
+// Segue abaixo um exemplo do uso de event.target:
+
+
+function resetText(event) {
+  // O Event é passado como um parâmetro para a função.
+  event.target.innerText = 'Opção reiniciada';
+  // O event possui várias propriedades, porém a mais usada é o event.target,
+  // que retorna o objeto que disparou o evento.
+}
+
+firstLi.addEventListener('dblclick', resetText);
+
+// Não precisa passar o parâmetro dentro da callback resetText. O próprio
+// navegador fará esse trabalho por você, não é legal? Desse jeito, o
+// event.target na nossa função retornará o objeto 'firstLi'.
