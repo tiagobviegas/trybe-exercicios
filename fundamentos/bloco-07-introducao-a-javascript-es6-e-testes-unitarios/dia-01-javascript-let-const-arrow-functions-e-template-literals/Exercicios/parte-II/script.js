70dcc27a9cb49d15d8d19e4c31d9e67694d230f3
tiagobviegas <tiagobviegas@gmail.com>
2022-04-17 20:52:19 -0300

    exercicio 1 pos aula refatorado

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-let-const-arrow-functions-e-template-literals/Exercicios/parte-II/script.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-let-const-arrow-functions-e-template-literals/Exercicios/parte-II/script.js
@@ -3,7 +3,7 @@
 //     Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
 
 const fatorial1 = (number) => {
-  for (let i = number; i > 2; i -= 1) {
+  for (let i = number; i > 1; i--) {
   number *= (i - 1);
   }
   return number;
@@ -13,7 +13,7 @@ console.log(fatorial1(6));
 
 //     Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator .
 
-const fatorial2 = (number) => number >= 2 ? fatorial2(number - 1) * number : 1; 
+const fatorial2 = (number) => number > 1 ? fatorial2(number - 1) * number : 1; 
 console.log(fatorial2(6));
 
 // 2 - Crie uma função que receba uma frase e retorne qual a maior palavra.
