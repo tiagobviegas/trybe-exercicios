1a8180a654c341950ff38a92a12b3de1704d9e7f
tiagobviegas <tiagobviegas@gmail.com>
2022-04-18 14:43:24 -0300

    atualizando repositorios com conteudo do dia 7.2

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte-I-adicionando-novas-chaves-keys/index.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte-I-adicionando-novas-chaves-keys/index.js
@@ -15,7 +15,7 @@ const community = 'Surrey, BC';
 student[newKey] = community;
 newKey = 'favoriteSubjects';
 const studies = {subject1: 'math', subject2: 'physics', subject3: 'geography', subject4: 'history',};
-student[newKey] = studies;
+student[newKey] = studies; 
 
 console.log(`${student['fullName']} nasceu em ${student.BornCity}, na rua ${student.address}, em ${student['Neighborghood']}.`);
 console.log(student.favoriteSubjects.subject2);
