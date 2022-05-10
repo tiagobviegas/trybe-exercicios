31676654695d59349ac2c6ab04a5ed698f8cd586
tiagobviegas <tiagobviegas@gmail.com>
2022-05-09 23:27:19 -0300

    adiciona arquivos das aulas do dia 8.2

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-hight-order-functions-foreach-find-some-every-sort/array-for-each.js
@@ -0,0 +1,8 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const multipliesFor2 = (element) => {
+  console.log(`${element} * 2: ${element * 2}`);
+};
+
+numbers.forEach(multipliesFor2);
+// A cada elemento do array, chamado pela função "numbers.forEach", a função "multipliesFor2" é chamada como parâmetro de "forEach". Este por sua vez interpreta que este parâmetro (por ser o primeiro) recebe os valores do array numbers. Assim, "element" carrega as informações de cada valor do array "numbers" para ser utilizado na tabuada de 2, exercida por "multipliesFor2";
