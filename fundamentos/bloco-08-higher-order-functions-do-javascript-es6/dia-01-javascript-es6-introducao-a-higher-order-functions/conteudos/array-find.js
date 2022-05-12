5968aba3b66b8fcad0debcd6d2dd34b33d1c3ce2
tiagobviegas <tiagobviegas@gmail.com>
2022-05-11 23:54:06 -0300

    exercicio 2 de Array.find concluido

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/conteudos/array-find.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/conteudos/array-find.js
@@ -1,18 +1,18 @@
  const numbers = [19, 21, 30, 3, 45, 22, 15];
-
- const verifyEven = (number) => number % 2 === 0;
- 
- const isEven = numbers.find(verifyEven);
- 
- console.log(isEven); // 30
- 
- console.log(verifyEven(9)); // False
- console.log(verifyEven(14)); // True
- 
- // Outra forma de ser realizada sem a necessidade de criar uma nova função.
- const isEven2 = numbers.find((number) => number % 2 === 0);
- 
- console.log(isEven2); // 30
+//
+// const verifyEven = (number) => number % 2 === 0;
+// 
+// const isEven = numbers.find(verifyEven);
+// 
+// console.log(isEven); // 30
+// 
+// console.log(verifyEven(9)); // False
+// console.log(verifyEven(14)); // True
+// 
+// // Outra forma de ser realizada sem a necessidade de criar uma nova função.
+// const isEven2 = numbers.find((number) => number % 2 === 0);
+// 
+// console.log(isEven2); // 30
 
 // A HOF "numbers.find" devolve apenas um valor booleano sobre o que foi passado.
 
@@ -31,9 +31,10 @@ console.log(findDivisibleBy3And5());
 
 
 // 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
-// const names = ['João', 'Irene', 'Fernando', 'Maria'];
-// 
-// const findNameWithFiveLetters = () => {
-// }
-// 
-// console.log(findNameWithFiveLetters());
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+ 
+const findNameWithFiveLetters = () => {
+  return names.find((name) => name.length === 5)
+}
+ 
+console.log(findNameWithFiveLetters());
