2217538a34f22ea431b1954a9317165dc5c975de
tiagobviegas <tiagobviegas@gmail.com>
2022-07-04 00:30:35 -0300

    continuando estudos de HOFs

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/conteudos/higher-order-functions.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/conteudos/higher-order-functions.js
@@ -1,3 +1,6 @@
+
+// Uma High Order Function é uma função que recebe como parâmetro outra função ou retorna um função. Este parâmetro em forma de função tem o nome técnico de "callback".
+
 // EXEMPLO CLÁSSICO DE HIGHER ORDER FUNCION: ADD EVENT LISTENER
 
 const button = document.querySelector('#signup-button');
