94d71bc6e229f46ee4ecbb4a1f17b5ec8341e0ec
tiagobviegas <tiagobviegas@gmail.com>
2022-07-04 09:42:28 -0300

    resolvendo criação de objetos da solucao do exercicio 1

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/conteudos/exercicios.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/conteudos/exercicios.js
@@ -2,17 +2,20 @@
 
 // 1 - Crie uma função que retorne um objeto no formato "{ nomeCompleto, email }" representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF "newEmployees" para criar cada pessoa contratada em seu respectivo "id". A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato "nome_da_pessoa@trybe.com".
 
-const newEmployees = () => {
+const newEmployees = (callback) => {
   const employees = {
-    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id4: callback('Ianny Mayara Oliveira Lopes Viegas'), // Nome: Ianny Mayara Oliveira Lopes Viegas -> Chame sua função passando o nome Ianny Mayara Oliveira Lopes Viegas como parâmetro, substituindo as aspas
   }
   return employees;
 };
 
 const entry = (nome) => {
-  return `${nome}, ${nome.toLowerCase().replaceAll(' ','_')}@trybe.com`;
-}
+  const name = nome
+  return { name, name.toLowerCase().replaceAll(' ','_')@trybe.com ;
+};
 
-console.log(entry('Pedro Guerra'));
+console.log(newEmployees(entry));
+console.log(newEmployees(entry).id1);
