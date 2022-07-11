15225d6be89984aca5d25893893791cec552f608
tiagobviegas <tiagobviegas@gmail.com>
2022-07-11 02:21:22 -0300

    exercicio 2 de Para Fixar resolvido

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-hight-order-functions-foreach-find-some-every-sort/arra-find.js
@@ -0,0 +1,46 @@
+// ARRAY FIND
+// Acha o primeiro elemento que satisfaça a condição passada e o retorna.
+
+// const numbers = [12, 4, 9, 43, 63, 54, 123, 54, 90, 0]
+
+// console.log(numbers.find((number)=> number > 30));
+
+//------------------------------------------------------------------
+
+// PARA FIXAR:
+// 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  // Adiciona seu código aqui
+  return numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+}
+
+// Construção reduzida:
+// const findDivisibleBy3And5 = () => numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+
+console.log(findDivisibleBy3And5())
+
+//------------------------------------------------------------------
+
+// 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+  // Construção tradicional:
+const findNameWithFiveLetters = () => {
+  // Adicione seu código aqui:
+  return names.find((name) => {
+    return name.split('').length === 5 ? name : ''
+  })
+}
+
+  // Construção alternativa:
+  // const findNameWithFiveLetters = () => {
+  //  return names.find((name) => name.split('').length === 5 ? name : '')
+  // }
+
+  // Construção reduzida:
+  // const findNameWithFiveLetters = () => names.find((name) => name.split('').length === 5 ? name : '')
+
+console.log(findNameWithFiveLetters());
