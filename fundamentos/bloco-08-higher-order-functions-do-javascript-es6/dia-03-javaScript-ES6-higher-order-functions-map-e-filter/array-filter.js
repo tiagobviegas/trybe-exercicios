871b81208039fefe1fe6f14923d7132aa67f0ae3
tiagobviegas <tiagobviegas@gmail.com>
2022-07-17 09:07:37 -0300

    estudos sobre array.map e array.filter concluidos

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javaScript-ES6-higher-order-functions-map-e-filter/array-filter.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javaScript-ES6-higher-order-functions-map-e-filter/array-filter.js
@@ -43,13 +43,10 @@ console.log(arr.map(newNames));
 
 // EXPERIMENTO: RETORNANDO A AVALIAÇÃO SE NOMES COMEÇAM COM A LETRA "L" OU NÃO USANDO ARRAY.MAP:
 
-const newNames2 = (name, i, arr) => {
-  return name.split('')[0] === 'L' ? 
-  `O nome "${arr[i]}" começa com a letra "L"` : 
-  `O nome "${arr[i]}" não começa com a letra "L"`
-};
+const namesStartsWithL = (name, i, arr) => (name.split('')[0] === 'L') ? (`O nome "${arr[i]}" começa com a letra "L"`) : 
+  (`O nome "${arr[i]}" não começa com a letra "L"`);
 
-console.log(arr.map(newNames2));
+console.log(arr.map(namesStartsWithL));
 // [
 //   'O nome "Tiago" não começa com a letra "L"',
 //   'O nome "Ianny" não começa com a letra "L"',
@@ -61,3 +58,74 @@ console.log(arr.map(newNames2));
 //   'O nome "Sheik" não começa com a letra "L"'
 // ]
 
+// AGORA A PRÁTICA
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// ------------------------------------------------------------------
+
+// Adicione o código do exercício aqui:
+
+// 1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+//    > Dica: Use a função map
