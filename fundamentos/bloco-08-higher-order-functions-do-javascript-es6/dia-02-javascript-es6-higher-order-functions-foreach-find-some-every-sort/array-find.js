829e819ceb7a8bd8830be36b824be36dfd35301c
tiagobviegas <tiagobviegas@gmail.com>
2022-08-27 09:42:30 -0300

    adiciona exercicio para fixar sobre array.find - 2 resolvido

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/array-find.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/array-find.js
@@ -13,3 +13,18 @@ console.log(findDivisibleBy3And5());
 
 //_________________________________________________________
 
+
+// 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  // Adicione seu código aqui:
+  return names.find((name) => name.length === 5);
+}
+
+console.log(findNameWithFiveLetters());
+
+//_________________________________________________________
+
+
