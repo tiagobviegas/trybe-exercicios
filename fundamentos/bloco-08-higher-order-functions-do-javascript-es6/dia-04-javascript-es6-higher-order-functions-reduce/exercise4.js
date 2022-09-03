e47f46010d909dc5aa7325bcdc30cce2d477725b
tiagobviegas <tiagobviegas@gmail.com>
2022-09-03 09:05:57 -0300

    exercicios de array.reduce - exercicio 4 em evolucao

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercise4.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercise4.js
@@ -66,3 +66,24 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+// 4 - Encontre o livro com o maior nome.
+
+
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
+function longestNamedBook() {
+  // escreva seu código aqui
+  return books.reduce((acc, curr) => acc.name.length > curr.name.length ? acc : curr)
+}
+
+console.log(longestNamedBook())
