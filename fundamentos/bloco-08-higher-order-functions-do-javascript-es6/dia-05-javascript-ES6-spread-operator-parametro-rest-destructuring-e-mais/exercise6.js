bbab7afb5e71056827e701aac1c2099e4df891fe
tiagobviegas <tiagobviegas@gmail.com>
2022-09-09 14:52:40 -0300

    exercicio 6 realizado

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-ES6-spread-operator-parametro-rest-destructuring-e-mais/exercise6.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-ES6-spread-operator-parametro-rest-destructuring-e-mais/exercise6.js
@@ -7,3 +7,7 @@ const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
 // escreva toObject abaixo
+
+const toObject = (...arrays) => ({ palio, shelbyCobra, chiron })
+
+console.log(toObject([palio, shelbyCobra, chiron]))
