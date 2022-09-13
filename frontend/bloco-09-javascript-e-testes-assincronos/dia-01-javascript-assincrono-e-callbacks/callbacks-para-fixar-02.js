69d49212106396c51e4e0f4ff8afeff21d593ae5
tiagobviegas <tiagobviegas@gmail.com>
2022-09-13 11:13:32 -0300

    exercicio 2 para fixar de callbacks cocluido

--- frontend/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/callbacks-para-fixar-02.js
+++ frontend/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/callbacks-para-fixar-02.js
@@ -1,11 +1,16 @@
-// callbacks-para-fixar-02.js
+// 2 - No código acima você tem a função getUser modificada, que agora funciona de modo assíncrono. Adicione uma callback como parâmetro da função getUser para realizar as operações abaixo:
+
+//    > A função getUser, ao ser chamada com o parâmetro userFullName, deve imprimir na tela: "Olá! Meu nome é Ivan Ivanovich"
+//    > A função getUser, ao ser chamada com o parâmetro userNationality, deve imprimir na tela: "Ivan é Russo"
+
+// Obs.: Analise o comportamento assíncrono da função getUser ao chamar getUser(userFullName) seguido de getUser(userNationality). Há momentos em que o nome da pessoa é impresso primeiro e outros em que a nacionalidade da pessoa é impressa primeiro!
 
 const userFullName = ({ firstName, lastName }) => `Olá! Meu nome é ${firstName} ${lastName}`;
 const userNationality = ({ firstName, nationality }) => `${firstName} é ${nationality}`;
 
 const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
 
-const getUser = (/* callback */) => {
+const getUser = (callback) => {
   setTimeout(() => {
     const user = {
       firstName: 'Ivan',
@@ -14,16 +19,9 @@ const getUser = (/* callback */) => {
     };
 
     // Dica: use esse `console.log()` abaixo para imprimir o resultado na tela.
-    console.log(user);
+    console.log(callback(user));
   }, delay());
 };
 
 getUser(userFullName); // deve imprimir "Olá! Meu nome é Ivan Ivanovich" depois de um certo tempo
 getUser(userNationality); // deve imprimir "Ivan é Russo" depois de um certo tempo
-
-
-// 2 - No código acima você tem a função getUser modificada, que agora funciona de modo assíncrono. Adicione uma callback como parâmetro da função getUser para realizar as operações abaixo:
-//    A função getUser, ao ser chamada com o parâmetro userFullName, deve imprimir na tela: "Olá! Meu nome é Ivan Ivanovich"
-//    A função getUser, ao ser chamada com o parâmetro userNationality, deve imprimir na tela: "Ivan é Russo"
-
-// Obs.: Analise o comportamento assíncrono da função getUser ao chamar getUser(userFullName) seguido de getUser(userNationality). Há momentos em que o nome da pessoa é impresso primeiro e outros em que a nacionalidade da pessoa é impressa primeiro!
