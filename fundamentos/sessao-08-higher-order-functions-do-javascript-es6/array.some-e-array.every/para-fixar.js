0ee38831993f21da582d0e8be69f4fc12b7cce73
Tiago Viegas <tiagobviegas@gmail.com>
2022-12-06 09:15:48 -0300

    exercicios de array.find e array.some para fixar, concluidos

---
+++ fundamentos/sessao-08-higher-order-functions-do-javascript-es6/array.some-e-array.every/para-fixar.js
@@ -0,0 +1,32 @@
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  //Adicione seu código aqui
+  return arr.some((nome) => {
+    return nome === name
+  })
+};
+
+console.log(hasName(names, 'Ana'));
+console.log(hasName(names, 'Pedro'));
+
+// ===============================================
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+  //Adicione seu código aqui
+const verifyAges = (arr, age) => {
+  return arr.every((person) => {
+    return person.age > age
+  })
+
+}
+
+console.log(verifyAges(people, 18));
+console.log(verifyAges(people, 14));
