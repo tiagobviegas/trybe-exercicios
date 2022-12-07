ac3df9291cfa5682eb16ddfe6fedc9bf456533ad
Tiago Viegas <tiagobviegas@gmail.com>
2022-12-06 22:57:59 -0300

    exercicio pos-aula corretor-automatico-de-exame concluido

--- fundamentos/sessao-08-higher-order-functions-do-javascript-es6/exercicios-agora-a-pratica/corretor-automatico-de-exame.js
+++ fundamentos/sessao-08-higher-order-functions-do-javascript-es6/exercicios-agora-a-pratica/corretor-automatico-de-exame.js
@@ -2,21 +2,21 @@ const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
 const result = (answers, grades, check) => {
-  return check(answers, grades)
-}
-
-const checkGrades = (answerSheet, studentSheet) => {
   let mark = 0;
-  answerSheet.forEach((answer) => studentSheet.forEach((grade) => {
-    if (answer === grade) {
-      mark += 1;
-    } else if (grade === 'N.A') {
-      mark += 0;
-    } else {
-      mark -= 0.5;
-    }
-  }))
-  return mark
+  for (let i = 0; i < answers.length; i += 1) {
+    mark += check(answers[i], grades[i])
+  }
+  return mark;
 }
 
-console.log(result(RIGHT_ANSWERS, STUDENT_ANSWERS, checkGrades));
+const answerCheck = (answers, grades) => {
+  if (answers === grades) {
+    return 1;
+  } else if (grades === 'N.A') {
+    return 0;
+  } else {
+    return -0.5;
+  }
+}
+
+console.log(result(RIGHT_ANSWERS, STUDENT_ANSWERS, answerCheck));
