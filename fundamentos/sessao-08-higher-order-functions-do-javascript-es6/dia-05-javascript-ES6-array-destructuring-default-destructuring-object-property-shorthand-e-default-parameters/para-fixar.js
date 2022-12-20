13b183f380b91a8405fc5237ba50a3cc0ab66c6a
Tiago Viegas <tiagobviegas@gmail.com>
2022-12-20 09:04:24 -0300

    exercicio 2 para fixar do dia 8.5 sobre array-descructuring concluido

--- fundamentos/sessao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-ES6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/para-fixar.js
+++ fundamentos/sessao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-ES6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/para-fixar.js
@@ -8,5 +8,19 @@ sum(primeNumbers[0], primeNumbers[2]) // 54
 
 // Produza o mesmo resultado acima, porém utilizando array destructuring
 
-const [firstNumber, , thirdNumber] =  primeNumbers
-sum(firstNumber, thirdNumber)
+const [firstNumber, , thirdNumber] = primeNumbers;
+sum(firstNumber, thirdNumber);
+
+// =======================================
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+[comida, animal, bebida] = ['arroz', 'gato', 'água']
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+// =======================================
