e224a660ccdee5bf25c29edf062045cba997c262
Tiago Viegas <tiagobviegas@gmail.com>
2022-12-24 12:20:24 -0300

    exercicio pos-aula parte II do dia 7.1 concluido

--- fundamentos/sessao-07-introducao-a-javascript-es6-e-testes-unitarios/agora-a-pratica/parte-II.js
+++ fundamentos/sessao-07-introducao-a-javascript-es6-e-testes-unitarios/agora-a-pratica/parte-II.js
@@ -36,14 +36,21 @@ const order = {
 
 const customerInfo = (order) => {
   // Adicione abaixo as informações necessárias.
-
+  const deliveryPerson = Object.values(order.order.delivery)[0]
+  const address = Object.values(order.address)
+  return console.log(`Olá ${deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${address[0]}, Nº: ${address[1]}, AP: ${address[2]}`)
 };
 
 customerInfo(order);
 
 const orderModifier = (order) => {
   // Adicione abaixo as informações necessárias.
-
+  const food = Object.keys(order.order.pizza);
+  const drink = order.order.drinks.coke.type;
+  const payment = order.payment;
+  Object.assign(order, { name: 'Luiz Silva' });
+  Object.assign(payment, { total: 50 })
+  return console.log(`Olá ${order.name}, o total do seu pedido de ${food[0]}, ${food[1]} e ${drink} é R$ ${payment.total}`)
 };
 
 orderModifier(order);
