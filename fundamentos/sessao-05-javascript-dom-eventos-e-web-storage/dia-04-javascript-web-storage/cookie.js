8c06f0a1a494c0ca872cdac88ca6c839c9752f40
Tiago Viegas <tiagobviegas@gmail.com>
2022-12-28 10:37:16 -0300

    adiciona respositorios para revisao da sessao 5

--- fundamentos/sessao-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/cookie.js
+++
@@ -1,8 +0,0 @@
-document.cookie = 'nome=tiago'
-// document.cookie = 'sobrenome=viegas'
-// document.cookie = 'idade=41'
-// document.cookie = 'cidade=Brasília'
-
-const myCookie = document.cookie
-
-console.log(myCookie)
