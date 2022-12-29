a9539f41d24c66dc156805ba8d44d6c4bf9d01d1
Tiago Viegas <tiagobviegas@gmail.com>
2022-12-29 07:23:47 -0300

    exercicios sobre seletores do dia 5.1 refatorado - WIP

--- fundamentos/sessao-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/seletores/script.js
+++ fundamentos/sessao-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/seletores/script.js
@@ -4,6 +4,8 @@ const emergencyTasksH3 = document.querySelectorAll('.emergency-tasks h3');
 const noEmergencyDiv = document.querySelectorAll('.no-emergency-tasks div');
 const noEmergencyTasks = document.querySelector('.no-emergency-tasks');
 const noEmergencyTasksH3 = document.querySelectorAll('.no-emergency-tasks h3');
+const headerBgColor = document.querySelector('#header-container');
+const footerBgColor = document.querySelector('#footer-container');
 
 const emergencyStyle = (tasksDiv, tasks, h3) => {
   for (let i = 0; i < tasksDiv.length; i += 1) {
@@ -21,5 +23,7 @@ const noEmergencyStyle = (tasksDiv, tasks, h3) => {
   tasks.style.backgroundColor = '#EFD446';
 };
 
+headerBgColor.style.backgroundColor = '#26B05C';
 emergencyStyle(emergencyDiv, emergencyTasks, emergencyTasksH3);
 noEmergencyStyle(noEmergencyDiv, noEmergencyTasks, noEmergencyTasksH3);
+footerBgColor.style.backgroundColor = '#0A462C';
