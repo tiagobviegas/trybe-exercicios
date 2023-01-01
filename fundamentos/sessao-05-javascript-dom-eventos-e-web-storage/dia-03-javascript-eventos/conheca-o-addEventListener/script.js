95bb8ce0d013b950dac23c1061bb94612c3d1523
Tiago Viegas <tiagobviegas@gmail.com>
2022-12-31 22:05:03 -0300

    item 1 dos exercicios sobre addEventListener concluido

--- fundamentos/sessao-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/conheca-o-addEventListener/script.js
+++ fundamentos/sessao-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/conheca-o-addEventListener/script.js
@@ -8,6 +8,8 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // - Copie esse arquivo e edite apenas ele;
 //  - Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
 
+/*  RESPOSTA: isso acontece devido à classe .tech que aplica o estilo transform: translateY(-20px);
+*/
 // - Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 //  - Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
@@ -23,16 +25,3 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // Segue abaixo um exemplo do uso de event.target:
 
-
-const resetText = (event) => {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
-}
-
-firstLi.addEventListener('dblclick', resetText);
-
-// Não precisa passar o parâmetro dentro da callback resetText. O próprio
-// navegador fará esse trabalho por você, não é legal? Desse jeito, o
-// event.target na nossa função retornará o objeto 'firstLi'.
