977a24fc6373c183369d4d9fbddc1bec8396927c
Tiago Viegas <tiagobviegas@gmail.com>
2023-01-07 09:18:09 -0300

    iniciada novos estudos sobre reduce

--- fundamentos/sessao-08-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/array.reduce/para-fixar1.js
+++
@@ -1,8 +0,0 @@
-const numbers = [50, 85, -30, 3, 15];
-
-const bigger = (acc, curr) => acc > curr ? acc : curr
-const result = numbers.reduce(bigger, 0)
-console.log(result)
-
-// resolvido em uma linha:
-// console.log(numbers.reduce((acc, curr) => acc > curr ? acc : curr, 0))
