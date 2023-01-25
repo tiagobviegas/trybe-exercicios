dacc34879679ee41b8eca0157f30fdea24e2329f
Tiago Viegas <tiagobviegas@gmail.com>
2023-01-25 15:17:12 -0300

    inicia estudos do dia 1.2

--- front-end/sessao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/estudos-e-anotacoes.js
+++ front-end/sessao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/estudos-e-anotacoes.js
@@ -2,5 +2,14 @@
 
 // Uma Promise retorna dois argumentos que são duas Funções. O primeiro parâmetro (que é uma função) é dedicado para uso de quando a promise é resolvida. E os segundo parâmetro (que é uma função!) para quando é rejeitada.
 
+// função que gera um número aleatório
+const generateRandomNumber = () => Math.round(Math.random() * 10);
 
-
+// promise resolvida retornando o número aleatório
+const resolvedPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = generateRandomNumber();
+      resolve(randomNumber);
+    }, 1000);
+  });
