a6f1c8efad0a12db5e0df5d82f3f8f487f23bd5c
Tiago Viegas <tiagobviegas@gmail.com>
2023-01-25 15:14:49 -0300

    inicia estudos do dia 1.2

---
+++ front-end/sessao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/estudos-e-anotacoes.js
@@ -0,0 +1,6 @@
+// new Promise((resolve, reject) => {}) // construtor
+
+// Uma Promise retorna dois argumentos que são duas Funções. O primeiro parâmetro (que é uma função) é dedicado para uso de quando a promise é resolvida. E os segundo parâmetro (que é uma função!) para quando é rejeitada.
+
+
+
