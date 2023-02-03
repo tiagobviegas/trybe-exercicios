7df534904407f340f1c869402012763e9905fdf0
Tiago Viegas <tiagobviegas@gmail.com>
2023-02-03 09:31:09 -0300

    inicia exercicios Casa de Câmbio

---
+++ front-end/sessao-01-introducao-ao-frontend-e-javascript-assincrono/dia-03-pratica-casa-de-cambio/src/main.js
@@ -0,0 +1,12 @@
+// import Swal from 'sweetalert2'; 
+
+const input = document.querySelector('#input-moeda');
+const btn = document.querySelector('button');
+const currency = document.querySelector('h2');
+const ul = document.querySelector('ul');
+
+// const BASE_API = `https://api.exchangerate.host/latest?base=${moeda}`;
+
+btn.addEventListener('click', () => {
+
+});
