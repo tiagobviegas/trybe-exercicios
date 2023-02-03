91317707fc9abf8f19e788c6f239f697d17b0e5f
Tiago Viegas <tiagobviegas@gmail.com>
2023-02-03 01:22:25 -0300

    exercicios corrigido conforme gabarito

--- front-end/sessao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/agora-a-pratica/api-de-super-herois/main.js
+++ front-end/sessao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/agora-a-pratica/api-de-super-herois/main.js
@@ -5,26 +5,22 @@ const heroImage = document.querySelector('#hero-image');
 const button = document.querySelector('#button');
 const acessToken = 5954222851266040;
 
+// O exercicio foi corrigido com orientação no gabarito
+
 // GERADOR DE IDs
 const heroesAmount = 750;
 const idNumberGen = () => Math.ceil((Math.random() * heroesAmount));
 
-const clearData = () => {
-  heroImage.removeAttribute('src');
-  heroName.innerHTML = '';
-};
-
 // API que retorna a imagem do herói. Também retorna o nome na chave "name"
-const HEROES_IMAGE_API = `https://www.superheroapi.com/api.php/${acessToken}/${idNumberGen()}/image`;
+const HEROES_IMAGE_API = `https://www.superheroapi.com/api.php/${acessToken}`;
 
 button.addEventListener('click', (event) => {
   event.preventDefault();
-  idNumberGen();
-  fetch(HEROES_IMAGE_API)
+  const rdmId = idNumberGen();
+  fetch(`${HEROES_IMAGE_API}/${rdmId}`)
     .then((response) => response.json())
     .then((data) => {
-      clearData();
-      heroImage.src = data.url;
+      heroImage.src = data.image.url;
       heroName.innerHTML = data.name;
     })
     .catch((error) => {
