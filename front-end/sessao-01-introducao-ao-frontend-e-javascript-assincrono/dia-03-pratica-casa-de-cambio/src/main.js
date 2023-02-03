f445b7e1e87f3a498858a9de7d0adc4d89ef2bdd
Tiago Viegas <tiagobviegas@gmail.com>
2023-02-03 10:03:37 -0300

    Casa de Câmbio - WIP | corrige fetch de API concluído

--- front-end/sessao-01-introducao-ao-frontend-e-javascript-assincrono/dia-03-pratica-casa-de-cambio/src/main.js
+++ front-end/sessao-01-introducao-ao-frontend-e-javascript-assincrono/dia-03-pratica-casa-de-cambio/src/main.js
@@ -3,13 +3,13 @@
 const input = document.querySelector('#input-moeda');
 const btn = document.querySelector('button');
 const currency = document.querySelector('h2');
-const ul = document.querySelector('ul');
+const ul = document.querySelector('#currencies');
 
 const getFetch = (moeda) => {
   const BASE_API = `https://api.exchangerate.host/latest?base=${moeda}`;
   return fetch(BASE_API)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => data);
 };
 
 btn.addEventListener('click', getFetch(input.value));
