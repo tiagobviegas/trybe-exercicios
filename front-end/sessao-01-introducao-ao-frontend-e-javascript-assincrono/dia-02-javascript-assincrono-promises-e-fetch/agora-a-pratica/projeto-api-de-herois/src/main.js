388da6cbb811b58252299276a41db66daefffaa1
Tiago Viegas <tiagobviegas@gmail.com>
2023-02-10 14:42:28 -0300

    iniciando o projeto

---
+++ front-end/sessao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/agora-a-pratica/projeto-api-de-herois/src/main.js
@@ -0,0 +1,17 @@
+// const paragrafo = document.querySelector('p');
+
+const heroImage = document.querySelector('#image');
+const heroName = document.querySelector('#hero-name');
+
+
+const myToken = 5954222851266040;
+
+const BASE_API = `https://superheroapi.com/api/${myToken}`;
+const idGen = Math.floor(Math.random() * 300)
+
+fetch(`${BASE_API}/${idGen}/image`)
+  .then((response) => response.json())
+  .then((data) => {
+    const { id, url } = data;
+
+  });
