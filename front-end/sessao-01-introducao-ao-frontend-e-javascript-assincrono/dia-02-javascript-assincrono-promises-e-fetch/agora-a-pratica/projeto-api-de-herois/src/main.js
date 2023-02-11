4ffde9af588f1f7fb11a4c91773302cbdb7c7618
Tiago Viegas <tiagobviegas@gmail.com>
2023-02-10 21:03:04 -0300

    adiciona exercicio do dia 1.2 concluído

--- front-end/sessao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/agora-a-pratica/projeto-api-de-herois/src/main.js
+++ front-end/sessao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/agora-a-pratica/projeto-api-de-herois/src/main.js
@@ -1,22 +1,15 @@
-// const paragrafo = document.querySelector('p');
+import { fetchBase } from '/utils/functions';
 
 const heroImage = document.querySelector('#image');
 const heroName = document.querySelector('#hero-name');
+const button = document.querySelector('#btn');
+const totalHeroes = 800;
 
-
-const myToken = 5954222851266040;
-
-const BASE_API = `https://superheroapi.com/api/${myToken}`;
-const idGen = Math.floor(Math.random() * 300)
-
-
-fetch(`${BASE_API}/${idGen}/image`)
-  .then((response) => response.json())
-  .then((data) => {
-    const { id, url } = data;
-    console.log(id)
-    console.log(url)
-    // heroImage.src = url
-    // heroName.innerHTML = id
-
-  });
+button.addEventListener('click', (event) => {
+  heroName.innerHTML = '';
+  heroImage.src = '';
+  event.preventDefault();
+  const idGen = Math.floor(Math.random() * totalHeroes);
+  const BASE_API = `https://akabab.github.io/superhero-api/api/id/${idGen}.json`;
+  fetchBase(BASE_API);
+});
