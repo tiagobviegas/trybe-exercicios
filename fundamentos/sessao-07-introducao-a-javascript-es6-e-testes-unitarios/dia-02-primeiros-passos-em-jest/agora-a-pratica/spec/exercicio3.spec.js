ebf858402a2900fab6773bc4be655bd061d1c295
Tiago Viegas <tiagobviegas@gmail.com>
2023-02-15 06:35:14 -0300

    item 3.1 do Exercício 3 pós-aula do dia 7.2 de Fundamentos concluído

---
+++ fundamentos/sessao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/agora-a-pratica/spec/exercicio3.spec.js
@@ -0,0 +1,10 @@
+const { encode, decode } = require('../exercicio3');
+
+describe('Testa as funções encode e decode', () => {
+
+  test('Se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente, na função encode()', () => {
+     expect(encode('a, e, i, o, u')).toBe('1, 2, 3, 4, 5');
+  });
+  
+  
+});
