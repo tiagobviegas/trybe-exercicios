23202b1d66406ec283ae9a9fb01493e9ac24317e
Tiago Viegas <tiagobviegas@gmail.com>
2023-02-14 22:07:51 -0300

    refatora item 1.3 do Exercício 1

--- fundamentos/sessao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/agora-a-pratica/spec/exercicio1.spec.js
+++ fundamentos/sessao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/agora-a-pratica/spec/exercicio1.spec.js
@@ -10,7 +10,7 @@ describe('Realiza testes da função myRemove', () => {
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
   });
 
-  it('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado.', () => {
+  it('Verifica se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado.', () => {
     expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
   });
 });
