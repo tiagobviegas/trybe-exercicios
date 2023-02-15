640e0521c0d12fffbcc01dd2811ce79615c0cf03
Tiago Viegas <tiagobviegas@gmail.com>
2023-02-15 06:47:24 -0300

    item 3.4 do Exercício 3 pós-aula do dia 7.2 de Fundamentos concluído

--- fundamentos/sessao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/agora-a-pratica/spec/exercicio3.spec.js
+++ fundamentos/sessao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/agora-a-pratica/spec/exercicio3.spec.js
@@ -3,16 +3,21 @@ const { encode, decode } = require('../exercicio3');
 describe('Testa as funções encode e decode', () => {
 
   test('Se encode e decode são funções;', () => {
-     expect(typeof encode).toBe('function');
-     expect(typeof decode).toBe('function');   
+    expect(typeof encode).toBe('function');
+    expect(typeof decode).toBe('function');   
   });
   
   test('Se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente, na função encode()', () => {
-     expect(encode('a, e, i, o, u')).toBe('1, 2, 3, 4, 5');
+    expect(encode('a, e, i, o, u')).toBe('1, 2, 3, 4, 5');
   });
   
-  test('Se a função decode, converte os números 1, 2, 3, 4 e 5 são convertidos nas vogais a, e, i, o, u, respectivamente;', () => {
-     expect(decode('1, 2, 3, 4, 5')).toBe('a, e, i, o, u')
+  test('Se a função decode, converte os números 1, 2, 3, 4 e 5 são convertidos nas vogais a, e, i, o, u, respectivamente', () => {
+    expect(decode('1, 2, 3, 4, 5')).toBe('a, e, i, o, u')
+  });
+  
+  test('Se as demais letras/números não são convertidos para cada caso;', () => {
+     expect(encode('Tiago')).toBe('T31g4');
+     expect(decode('T31g4')).toBe('Tiago');
   });
   
 
