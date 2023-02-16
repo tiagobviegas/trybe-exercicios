d9217c48ea4ed6fa67973094a78fe61e83e97e84
Tiago Viegas <tiagobviegas@gmail.com>
2023-02-15 22:11:44 -0300

    Requisito "Teste se a função createItem lança algum erro quando não recebe parâmetros." dos exercícios de fixação do dia 7.3 concluído

--- fundamentos/sessao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
+++ fundamentos/sessao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
@@ -2,12 +2,14 @@ const createItem = require('./createItem');
 describe('a função createItem', () => {
   it('cria um item válido', () => {
     expect(createItem('banana', 'kg', 1.99, 20))
-      .toEqual({ name: 'banana', quantity: 20, unit: 'kg', price: 1.99 })
+      .toEqual({ name: 'banana', quantity: 20, unit: 'kg', price: 1.99 });
   });
   it('utiliza zero como quantidade padrão', () => {
-    expect(createItem('banana', 'kg', 1.99)).toHaveProperty('quantity', 0)
+    expect(createItem('banana', 'kg', 1.99)).toHaveProperty('quantity', 0);
   });
-  it.todo('Lança um erro quando não recebe parâmetros');
+  it('Lança um erro quando não recebe parâmetros', () => {
+    expect(() => { createItem() }).toThrow();
+  });
   it.todo('Lança um erro se o nome do item não é uma string');
   it.todo('Lança um erro se o preço é negativo');
   it.todo('Lança um erro se o preço é zero');
