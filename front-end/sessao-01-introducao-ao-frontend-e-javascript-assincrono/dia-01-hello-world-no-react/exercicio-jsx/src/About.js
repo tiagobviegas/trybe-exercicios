f2061221ee7081b29490022abae1cebb392595b1
Tiago Viegas <tiagobviegas@gmail.com>
2023-05-14 11:24:23 -0300

    adiciona exercicio de fixação do dia 3.1

---
+++ front-end/sessao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-hello-world-no-react/exercicio-jsx/src/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Tiago Viegas</h1>
+        <p>Desenvolvedor web formado pela Trybe, disponível no mercado para contrato imediato, no Brasil ou no exterior!</p>
+        
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>HTML</li>
+          <li>JavaScript</li>
+          <li>CSS</li>
+          <li>Bootstrap</li>
+          <li>API</li>
+          <li>React</li>
+        </ul>
+      </>
+    )
+  }
+}
+
+export default About;
