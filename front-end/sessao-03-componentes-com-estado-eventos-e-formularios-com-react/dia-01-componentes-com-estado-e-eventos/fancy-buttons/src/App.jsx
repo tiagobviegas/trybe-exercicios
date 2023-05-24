7a549b4d7e2ca370677af2564818db69cd62b62d
Tiago Viegas <tiagobviegas@gmail.com>
2023-05-24 04:24:52 -0300

    exercicio de fixação "fancy-buttons" concluído

--- front-end/sessao-03-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.jsx
+++ front-end/sessao-03-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
+const handleClick1 = () => console.log('Fui clicado 1!')
+const handleClick2= () => console.log('Fui clicado 2!')
+const handleClick3 = () => console.log('Fui clicado 3!')
 
 class App extends React.Component {
   render() {
-    const handleClick1 = () => console.log('Fui clicado 1!')
-    const handleClick2= () => console.log('Fui clicado 2!')
-    const handleClick3 = () => console.log('Fui clicado 3!')
     return (
       <>
         <button onClick={handleClick1}>Clicar 1</button>
