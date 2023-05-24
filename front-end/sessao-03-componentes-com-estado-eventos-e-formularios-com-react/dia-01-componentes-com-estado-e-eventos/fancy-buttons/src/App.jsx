6d878b448983998fadaf791ff82f3ae39714fef6
Tiago Viegas <tiagobviegas@gmail.com>
2023-05-24 18:40:14 -0300

    exercicios de fixação state vs props concluídos

--- front-end/sessao-03-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.jsx
+++ front-end/sessao-03-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.jsx
@@ -1,40 +1,6 @@
 import React from 'react';
 
 class App extends React.Component {
-  /*
-  Utilizando o Constructor juntamente com "bind" e "this"
-  constructor() {
-    super()
-    
-    this.state = {
-      counter1: 0,
-      counter2: 0,
-      counter3: 0,
-    }
-
-    this.handleClick1 = this.handleClick1.bind(this)
-    this.handleClick2 = this.handleClick2.bind(this)
-    this.handleClick3 = this.handleClick3.bind(this)  
-  }
-
-  handleClick1() {
-    this.setState((prevState) => ({
-      counter1: prevState.counter1 + 1
-    }))
-  }
-
-  handleClick2() {
-    this.setState((prevState) => ({
-      counter2: prevState.counter2 + 1
-    })) 
-  }
-
-  handleClick3() {
-    this.setState((prevState) => ({
-      counter3: (prevState.counter3 + 1)
-    }))
-  }
-  */
 
   state = {
     counter1: 0,
@@ -42,17 +8,30 @@ class App extends React.Component {
     counter3: 0,
   }
 
-  handleClick1 = () => this.setState((prevState) => ({
-    counter1: prevState.counter1 + 1
-  }))
+  evenCheck = (number) => number % 2 === 0 ? 'green' : 'white'
 
-  handleClick2 = () => this.setState((prevState) => ({
-    counter2: prevState.counter2 + 1
-  }))
+  handleClick1 = () => {
+    const { counter1 } = this.state;
+    
+    this.setState((prevState) => ({
+      counter1: prevState.counter1 + 1
+    }), () => console.log(`Botão 1 ${this.evenCheck(counter1)}`))
+  };
 
-  handleClick3 = () => this.setState((prevState) => ({
-    counter3: prevState.counter3 + 1
-  }))
+  handleClick2 = () => {
+    const { counter2 } = this.state;
+
+    this.setState((prevState) => ({
+      counter2: prevState.counter2 + 1
+    }), () => console.log(`Botão 2 ${this.evenCheck(counter2)}`))
+  }
+
+  handleClick3 = () => {
+    const { counter3 } = this.state;
+    this.setState((prevState) => ({
+      counter3: prevState.counter3 + 1
+    }), () => console.log(`Botão 3 ${this.evenCheck(counter3)}`))
+  }
 
   render() {
     const {
@@ -63,13 +42,25 @@ class App extends React.Component {
 
     return (
       <div>
-        <button type="button" onClick={this.handleClick1}>
+        <button
+          type="button"
+          onClick={this.handleClick1}
+          style={ { backgroundColor: this.evenCheck(counter1) } }
+        >
           { `Cliques no Botão 1: ${counter1}` }
         </button>
-        <button type="button" onClick={this.handleClick2}>
+        <button
+          type="button"
+          onClick={this.handleClick2}
+          style={ { backgroundColor: this.evenCheck(counter2) }}
+        >
           { `Cliques no Botão 2: ${counter2}` }
         </button>
-        <button type="button" onClick={this.handleClick3}>
+        <button
+          type="button"
+          onClick={this.handleClick3}
+          style={ { backgroundColor: this.evenCheck(counter3)}}
+        >
           { `Cliques no Botão 3: ${counter3}` }
         </button>
       </div>
