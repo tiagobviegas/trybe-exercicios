9b8f412c3d7ddea30c81e8a18b017716ef39a668
Tiago Viegas <tiagobviegas@gmail.com>
2023-09-13 01:25:22 -0300

    refatorando exercícios de revisão

--- front-end/1-periodo/sessao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/01-configurando-o-ambiente-de-desenvolvimento/src/main.js
+++ front-end/1-periodo/sessao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/01-configurando-o-ambiente-de-desenvolvimento/src/main.js
@@ -2,8 +2,13 @@ import { nanoid } from 'nanoid';
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
+const limparBtn = document.querySelector('#clear');
 
 passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
+});
+
+limparBtn.addEventListener('click', () => {
+  displayPasswordEl.innerHTML = '';
 });
